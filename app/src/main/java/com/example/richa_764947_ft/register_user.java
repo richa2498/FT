1622a970975be03b11ac0f2c84c8e07bb625fbcb
richa <Richa1998.patel@gmail.como>
2019-12-14 12:21:24 -0500

    final test done

---
+++ app/src/main/java/com/example/richa_764947_ft/register_user.java
@@ -0,0 +1,69 @@
+package com.example.richa_764947_ft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.Random;
+
+public class register_user extends AppCompatActivity {
+
+    EditText nameM,emailM,phoneM;
+    Button saveData;
+    UserData userData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+        nameM = findViewById(R.id.name);
+        emailM = findViewById(R.id.email);
+        phoneM = findViewById(R.id.phone);
+        saveData = findViewById(R.id.register);
+
+
+
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if(nameM.getText().toString().isEmpty() || emailM.getText().toString().isEmpty() || phoneM.getText().toString().isEmpty() ){
+                    //Toast.makeText(register_user.this,"all field must be filled",Toast.LENGTH_SHORT).show();
+                   AlertDialog alertDialog = new AlertDialog.Builder(
+                           register_user.this).create();
+                   System.out.println("git hub not working");
+//                   // Setting Dialog Title
+                  alertDialog.setTitle("LOOK");
+
+                   // Setting Dialog Message
+                   alertDialog.setMessage("Fields are Empty");
+
+                   // Setting Icon to Dialog
+                   alertDialog.setIcon(R.drawable.checked);
+                   alertDialog.show();
+               }
+               else {
+                    Random ran = new Random();
+                    int nxt = ran.nextInt(20);
+
+                    userData = new UserData(nameM.getText().toString(), emailM.getText().toString(), phoneM.getText().toString(),UserData.imageIcons[nxt]);
+                    Intent intent = new Intent(register_user.this,verification.class);
+                    intent.putExtra("userData", userData);
+                    startActivity(intent);
+                    finish();
+
+
+
+
+
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/richa_764947_ft/user_data.java
@@ -0,0 +1,94 @@
+package com.example.richa_764947_ft;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+interface Listener {
+    void onItemClick(int id);
+}
+public class user_data extends Fragment {
+
+    private Listener listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (Listener) context;
+    }
+
+    public user_data() {
+        // Required empty public constructor
+    }
+
+    userDisplayAdapter ua;
+    View view;
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_user_data, container, false);
+
+
+
+        return view;
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Button add;
+        ListView lv;
+        lv = view.findViewById(R.id.view_data);
+        if(!UserData.data.isEmpty()) {
+//            String[] name = new String[UserData.data.size()];
+//            for (int i = 0; i < name.length; i++) {
+//                name[i] = UserData.data.get(i).getName();
+//            }
+//
+//            ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, name);
+//            lv.setAdapter(adapter);
+
+            ua = new userDisplayAdapter(getContext(),R.layout.layout_list,UserData.data);
+            lv.setAdapter(ua);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    if (listener != null)
+                        listener.onItemClick(i);
+
+                }
+            });
+
+        }
+        add = view.findViewById(R.id.add_user);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), register_user.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
+
